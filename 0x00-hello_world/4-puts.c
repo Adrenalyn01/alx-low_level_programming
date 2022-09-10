b62dcb64ab5a81e38a0316c95b9ebf21cb2e57f8
Adrenalyn <legitaizy2020@gmail.com>
2022-09-10 11:00:25 -0700

    betty issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-*
 *main - print string function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle"
 *Return: returns 0 if there are  no errors, else returns non-zero value
 */
-
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
