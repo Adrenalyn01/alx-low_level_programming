6f62f7fe5de915517c0b917e7b9c93cc13198fc0
Adrenalyn <legitaizy2020@gmail.com>
2022-09-10 08:57:30 -0700

    printable output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+
+*main - print string function
+*Return: returns 0 if there are  no errors, else returns non-zero value
+*/
+
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}
